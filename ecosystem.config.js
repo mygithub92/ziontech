f47c686e90861f61d020a87047a053cb47e529c2
David <linlwangster@gmail.com>
2018-04-12 21:50:13 +0930

    Add pm2 ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps : [{
+      name        : "server",
+      script      : "./server.js",
+      watch       : true,
+      env: {
+        "NODE_ENV": "production",
+        "PORT":"80"
+      }
+    }]
+  }
