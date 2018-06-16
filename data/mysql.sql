5c5942dac9ae68cd6379a820a33aae22fa2bf649
linlwangster@gmail.com <linlwangster@gmail.com>
2018-06-16 16:42:25 +0930

    local build

--- data/mysql.sql
+++ data/mysql.sql
@@ -1,32 +1,32 @@
-LOAD DATA LOCAL INFILE  'C:\\Users\\diw389\\work\\ziontech\\data\\role.csv'
+LOAD DATA LOCAL INFILE  'C:\\data\\role.csv'
 INTO TABLE `role`
 FIELDS OPTIONALLY ENCLOSED BY '"'
 TERMINATED BY ',' 
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE  'C:\\Users\\diw389\\work\\ziontech\\data\\user.csv'
+LOAD DATA LOCAL INFILE  'C:\\data\\user.csv'
 INTO TABLE `user`
 FIELDS OPTIONALLY ENCLOSED BY '"'
 TERMINATED BY ',' 
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE  'C:\\Users\\diw389\\work\\ziontech\\data\\stage.csv'
+LOAD DATA LOCAL INFILE  'C:\\data\\stage.csv'
 INTO TABLE `stage`
 FIELDS OPTIONALLY ENCLOSED BY '"'
 TERMINATED BY ',' 
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE  'C:\\Users\\diw389\\work\\ziontech\\data\\product.csv'
+LOAD DATA LOCAL INFILE  'C:\\data\\product.csv'
 INTO TABLE `product`
 FIELDS OPTIONALLY ENCLOSED BY '"'
 TERMINATED BY ',' 
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE  'C:\\Users\\diw389\\work\\ziontech\\data\\grape.csv'
+LOAD DATA LOCAL INFILE  'C:\\data\\grape.csv'
 INTO TABLE `grape`
 FIELDS OPTIONALLY ENCLOSED BY '"'
 TERMINATED BY ',' 
