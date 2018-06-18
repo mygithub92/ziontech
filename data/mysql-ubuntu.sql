9486279502864e0202927268ea8ab52f0caaef99
mygithub92 <linlwangster@gmail.com>
2018-06-18 22:51:19 +0930

    driver

--- data/mysql-ubuntu.sql
+++ data/mysql-ubuntu.sql
@@ -12,6 +12,13 @@ TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
+LOAD DATA LOCAL INFILE  '~/fabric/education/LFS171x/fabric-material/ziontech/data/user_role.csv'
+INTO TABLE `user_role`
+FIELDS OPTIONALLY ENCLOSED BY '"'
+TERMINATED BY ',' 
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
 LOAD DATA LOCAL INFILE  '~/fabric/education/LFS171x/fabric-material/ziontech/data/stage.csv'
 INTO TABLE `stage`
 FIELDS OPTIONALLY ENCLOSED BY '"'
